588251395312ea949d8ec4e5d8ca5f116bb7ab8b
Gayan Kumarage <gayan_ac@hotmail.com>
2020-03-08 20:32:58 -0500

    live reloading enabled with nodemon and ts-node

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ const APP_SERVER = express();
 
 export default (): void => {
 
-    APP_SERVER.get('/', (req, res) => res.send('Hola! 🙋🏻‍♂️'));
+    APP_SERVER.get('/', (req, res) => res.send('Hola!! 🙋🏻‍♂️'));
 
     APP_SERVER.listen(
         { port: 3000 },
-        (): void => console.log(`\n 🚀 running on port 3000`));
+        (): void => console.log(`\n 🚀 running on port :: 3000`));
 
 }
