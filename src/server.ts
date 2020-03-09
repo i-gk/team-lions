63dbb04a3cc85eaf0627035e407eef20092a5331
Gayan Kumarage <gayan_ac@hotmail.com>
2020-03-08 21:05:28 -0500

    using mongoose lib for mongodb operations

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import {  ApolloServer } from 'apollo-server-express';
+import mongoose from 'mongoose';
 
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
@@ -10,12 +11,17 @@ const SERVER: ApolloServer = new ApolloServer({
     resolvers
 });
 
-export default (): void => {
+export default async (): Promise<void> => {
 
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/devtest', { useNewUrlParser: true, useUnifiedTopology: true })
 
-    SERVER.applyMiddleware({app: APP, path: '/'});
-    APP.listen(
-        { port: 3000 },
-        (): void => console.log(`\n 🚀 running on port :: 3000`));
+        SERVER.applyMiddleware({app: APP, path: '/'});
+        APP.listen(
+            { port: 3000 },
+            (): void => console.log(`\n 🚀 running on port :: 3000`));
+    } catch (e) {
+        console.error('Failed to start the server ', e)
+    }
 
 }
