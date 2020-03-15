5df697c5b150a1fb3aa77936e5b633be2b29d339
Gayan Kumarage <gayan_ac@hotmail.com>
2020-03-15 13:53:43 -0500

    mongodb is now authenticated. All authentication details and mongodb instance information are fetched from ENV variables.

--- src/server.ts
+++ src/server.ts
@@ -5,21 +5,31 @@ import mongoose from 'mongoose';
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
 
+const PORT = process.env.PORT || 3000;
+const IS_DEVELOPMENT = (process.env.NODE_ENV === 'development');
+const MONGO_USER = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PSWD;
+const MONGO_IP = process.env.MONGO_IP;
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+
 const APP = express();
 const SERVER: ApolloServer = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    playground: IS_DEVELOPMENT
 });
 
 export default async (): Promise<void> => {
 
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/devtest', { useNewUrlParser: true, useUnifiedTopology: true })
+        await mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/${MONGO_DB_NAME}`, 
+                    { useNewUrlParser: true, useUnifiedTopology: true })
 
         SERVER.applyMiddleware({app: APP, path: '/'});
         APP.listen(
-            { port: 3000 },
-            (): void => console.log(`\n 🚀 running on port :: 3000`));
+            { port: PORT },
+            (): void => console.log(`\n 🚀 running on port :: ${PORT}`));
     } catch (e) {
         console.error('Failed to start the server ', e)
     }
